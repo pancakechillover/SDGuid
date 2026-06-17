2dfd6600bb07fb2e29c857fd35a7118376109040
Karakan <pankechill@outlook.com>
2026-06-17 22:53:27 +0800

    ok

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,4 +1,21 @@
 import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
 import './style.css'
 
-export default DefaultTheme
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'doc-after': () => h('footer', { class: 'custom-doc-footer-container' }, [
+        h('div', { class: 'custom-doc-footer-inner' }, [
+          h('span', null, "Scholar's Dungeon："),
+          h('a', {
+            href: 'https://scholars-dungeon.karakan.top/',
+            target: '_blank',
+            rel: 'noopener noreferrer'
+          }, 'https://scholars-dungeon.karakan.top/')
+        ])
+      ])
+    })
+  }
+}
